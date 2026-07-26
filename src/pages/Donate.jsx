37cbc73448ea90b7797ff2e859b37912c62b8922
Neema931 <neemaisabel@gmail.com>
2026-07-26 22:57:27 +0300

    .

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -29,7 +29,7 @@ function Donate() {
         }
 
         try {
-            const response = await fetch(`${apiUrl}/api/donate`, {
+            const response = await fetch(`${apiUrl}/donate`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
