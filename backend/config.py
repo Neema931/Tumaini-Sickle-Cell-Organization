a29da7cd522557d56e1b8fac4a2003798e6d163e
Neema931 <neemaisabel@gmail.com>
2026-07-26 20:09:22 +0300

    deployment

--- backend/config.py
+++ backend/config.py
@@ -6,11 +6,10 @@ load_dotenv()
 class Config:
     PESAPAL_CONSUMER_KEY = os.getenv("PESAPAL_CONSUMER_KEY")
     PESAPAL_CONSUMER_SECRET = os.getenv("PESAPAL_CONSUMER_SECRET")
-
+    FRONTEND_URL = "https://tumaini-sickle-cell-organization.vercel.app/"
     ENVIRONMENT = "sandbox"
 
     CALLBACK_URL = "http://localhost:5173/payment-success"
 
-    print("KEY:", PESAPAL_CONSUMER_KEY)
-    print("SECRET:", PESAPAL_CONSUMER_SECRET)
+    
     
